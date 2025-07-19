2f550247dfd6e22ad35b2a59679a4040121d98f6
Raphaël <35407363+EniumRaphael@users.noreply.github.com>
2025-07-19 23:44:04 +0200

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git clone https://github.com/iadjedj/onboarding_camera_script ./tmp
+
+mv ./tmp/* ./ && rm -rf ./tmp
+
+python3 -m venv ./.venv
+
+source ./.venv/bin/activate
+
+pip install -r requirements.txt
+
+python3 main.py
